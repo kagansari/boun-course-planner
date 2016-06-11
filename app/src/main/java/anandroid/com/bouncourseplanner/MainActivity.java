89fba11e8fcfdd71ca4a1c8a168c21699bef2cea
Kagan Sari <kagan.sari@getir.com>
2016-06-12 02:15:44 +0300

    tabs added

--- app/src/main/java/anandroid/com/bouncourseplanner/MainActivity.java
+++ app/src/main/java/anandroid/com/bouncourseplanner/MainActivity.java
@@ -1,13 +1,30 @@
 package anandroid.com.bouncourseplanner;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    public ViewPager viewPager;
+    public PageAdapter pageAdapter;
+    public TabLayout tabLayout;
+
+    private void initTabs() {
+        viewPager = (ViewPager) findViewById(R.id.pager);
+        tabLayout = (TabLayout) findViewById(R.id.tabs);
+        pageAdapter = new PageAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(pageAdapter);
+
+        tabLayout.setupWithViewPager(viewPager);
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initTabs();
     }
 }
---
+++ app/src/main/java/anandroid/com/bouncourseplanner/PageAdapter.java
@@ -0,0 +1,44 @@
+package anandroid.com.bouncourseplanner;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+/**
+ * Created by tv on 11/06/16.
+ */
+public class PageAdapter extends FragmentPagerAdapter {
+
+    public static final String key = "object";
+
+    public PageAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public int getCount() {
+        return 2;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        switch (position) {
+            case 0:
+                return new SearchFragment();
+            case 1:
+                return new ScheduleFragment();
+        }
+        return null;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        switch (position) {
+            case 0:
+                return "Search";
+            case 1:
+                return "Schedule";
+        }
+        return null;
+    }
+}
