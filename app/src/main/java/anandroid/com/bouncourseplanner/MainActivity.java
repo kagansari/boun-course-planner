ab239088ff9d10fb76adc274ab21d40d250c667b
Kagan Sari <kagan.sari@getir.com>
2016-06-12 12:18:17 +0300

    drawer added

--- app/src/main/java/anandroid/com/bouncourseplanner/MainActivity.java
+++ app/src/main/java/anandroid/com/bouncourseplanner/MainActivity.java
@@ -3,22 +3,42 @@ package anandroid.com.bouncourseplanner;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
     public ViewPager viewPager;
     public PageAdapter pageAdapter;
     public TabLayout tabLayout;
+    public LinearLayout leftDrawer;
+    public DrawerLayout drawerLayout;
+    public ActionBarDrawerToggle drawerToggle;
 
     private void initTabs() {
         viewPager = (ViewPager) findViewById(R.id.pager);
         tabLayout = (TabLayout) findViewById(R.id.tabs);
         pageAdapter = new PageAdapter(getSupportFragmentManager());
         viewPager.setAdapter(pageAdapter);
-
         tabLayout.setupWithViewPager(viewPager);
+    }
 
+    private void initDrawer() {
+        leftDrawer = (LinearLayout) findViewById(R.id.leftDrawer);
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+        drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.course, R.string.course);
+        drawerLayout.addDrawerListener(drawerToggle);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        drawerToggle.onOptionsItemSelected(item);
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
@@ -26,5 +46,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initTabs();
+        initDrawer();
     }
 }
